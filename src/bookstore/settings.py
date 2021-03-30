28bdadc0e9e1b7e55132e86aba9bd16e5d7a7af9
Daniel MacDonald <dam96271@uga.edu>
2021-03-30 17:01:13 -0400

    Bread time

--- src/bookstore/settings.py
+++ src/bookstore/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #created
-    'pages'
+    'pages',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- src/pages/models.py
+++ src/pages/models.py
@@ -11,18 +11,18 @@ class Book(models.Model):
 		db_table = 'Books'
 
 class User(models.Model):
-	first_name = models.CharField(max_length=25)
-	last_name = models.CharField(max_length=25)
-	phone_num = models.CharField(max_length=11)
-	user_email = models.EmailField(max_length=50)
-	user_pass = models.CharField(max_length=50)
-	user_street = models.CharField(max_length=50)
-	user_city = models.CharField(max_length=50)
-	user_state = models.CharField(max_length=2)
-	user_zip = models.CharField(max_length=10)
-	user_card_num = models.CharField(max_length=50)
-	user_card_exp = models.DateField()
-	user_card_seccode = models.CharField(max_length=3)
+	first_name = models.CharField(max_length=25, null=True)
+	last_name = models.CharField(max_length=25, null=True)
+	phone_num = models.CharField(max_length=11, null=True)
+	user_email = models.EmailField(max_length=50, primary_key=True)
+	user_pass = models.CharField(max_length=50, null=True)
+	user_street = models.CharField(max_length=50, null=True)
+	user_city = models.CharField(max_length=50, null=True)
+	user_state = models.CharField(max_length=2, null=True)
+	user_zip = models.CharField(max_length=10, null=True)
+	user_card_num = models.CharField(max_length=50, null=True)
+	user_card_exp = models.DateField(null=True)
+	user_card_seccode = models.CharField(max_length=3, null=True)
 
 	class Meta():
 		db_table = "Users"
