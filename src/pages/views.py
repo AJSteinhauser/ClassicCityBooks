630a5718047b98c4dad3e6211d86c99766a9af16
Daniel MacDonald <dam96271@uga.edu>
2021-04-27 17:27:24 -0400

    Fixed new promotion page

--- src/pages/views.py
+++ src/pages/views.py
@@ -614,6 +614,7 @@ def promotions_view(request, *args, **kwargs):
 def newpromotion_view(request, *args, **kwargs):
     if not checkAdminStatus(request, *args, **kwargs):
         return homepage_view(request, *args, **kwargs);
+    form = newpromotion()
     if request.method =="POST":
         form = newpromotion(request.POST)
         if form.is_valid():
